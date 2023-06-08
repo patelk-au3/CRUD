6bbce96b1e11f0a37659c16718999976d8ed1a09
patelk-au3 <patelk8182@gmail.com>
2023-06-08 12:18:03 +0530

    React-CRUD-operation

---
+++ todolist/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>TO-DO-LIST</h1>
+      <input type='text' placeholder='Enter The Task'></input><button className='btn-primary'>Submit</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
