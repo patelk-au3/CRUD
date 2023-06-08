6c6b868baef39b3ff6ffbd6332f041dc92b0b151
patelk-au3 <patelk8182@gmail.com>
2023-06-08 12:45:51 +0530

    React-todo-operation

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
-
-  
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>TO-DO-LIST</h1>
-      <input type='text' placeholder='Enter The Task'></input><button className='btn-primary'>Submit</button>
-      </header>
+      <TodoWrapper  />
     </div>
   );
 }
